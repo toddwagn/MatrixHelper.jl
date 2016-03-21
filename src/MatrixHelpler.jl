2c67fa8de28f859b8bd02b30380871430e75149a
Todd A Wagner <twagner@alumni.usc.edu>
2016-03-21 15:30:22 -0700

    Initial commit of MatrixHelper

---
+++ src/MatrixHelpler.jl
@@ -0,0 +1,38 @@
+# This contains matrix helper functions
+# License is MIT: http://julialang.org/license
+
+module MatrixHelper
+
+export # extend Base.Sort functions to better support Matricies
+	premcols,
+	permrows,
+	selectpermcols,
+	selectpermrows
+
+## 
+
+function permcols(A::AbstractMatrix; kws...)
+	r = 1:size(A,1)
+	cols = [ sub(A,r,i) for i=1:size(A,2) ]
+	sortperm(cols; kws..., order=Base.Order.Lexicographic)
+end
+
+function permrows(A::AbstractMatrix; kws...)
+	c = 1:size(A,2)
+	rows = [ sub(A,i,c) for i=1:size(A,1) ]
+	sortperm(rows; kws..., order=Base.Order.Lexicographic)
+end
+
+function selectpermcols(A::AbstractMatrix; kws...)
+	r = 1:size(A,1)
+	cols = [ sub(A,r,i) for i=1:size(A,2) ]
+	selectperm(cols; kws..., order=Base.Order.Lexicographic)
+end
+
+function selectpermrows(A::AbstractMatrix, k::Union{Integer,OrdinalRange}; kws...)
+	c = 1:size(A,2)
+	rows = [ sub(A,i,c) for i=1:size(A,1) ]
+	selectperm(rows, k; kws..., order=Base.Order.Lexicographic)
+end
+
+end # Module
